4a3eec63dffe2658dd02a4863162be27c23f710a
MathematicusLucian <52294958+MathematicusLucian@users.noreply.github.com>
2024-12-27 04:39:42 +0000

    Tidied the fetch of patientProfile, and patientGenome tables (IndexedDB); and bulkPut logic

--- database/db.ts
+++ database/db.ts
@@ -49,12 +49,12 @@ interface IFullReport {
 }
 
 const patientsIndexedDb = new Dexie('PatientDatabase') as Dexie & {
-  patientprofile: EntityTable<
+  patientProfile: EntityTable<
   IPatientProfile,
       'patient_id' // primary key "id" (for the typings only)
   >;
 } & {
-  patientgenome: EntityTable<
+  patientGenome: EntityTable<
   IPatientGenome,
       'rsid' // primary key "id" (for the typings only)
   >;
@@ -62,14 +62,64 @@ const patientsIndexedDb = new Dexie('PatientDatabase') as Dexie & {
 
 // Schema declaration:
 // "patientProfile": primary key "id" 
-patientsIndexedDb.version(1).stores({patientprofile: "++patient_id, patient_name", patientgenome: "rsid, patient_id, chromosome, position, genotype"});
+patientsIndexedDb.version(1).stores({
+  patientProfile: "++patient_id, patient_name", 
+  patientGenome: "rsid, patient_id, chromosome, position, genotype"
+});
 patientsIndexedDb.open();
 // Output the schema of each table:
 patientsIndexedDb.tables.forEach(function (table) {
-    // console.log("Schema of " + table.name + ": " + JSON.stringify(table.schema));
+    console.log("Schema of " + table.name + ": " + JSON.stringify(table.schema));
 }); 
 
-export type { IPatientProfile, IPatientGenome, IPatientProfileAndGenome, ISnpPairsResearch, IFullReport};
-export const patientProfileTable = patientsIndexedDb.table('patientprofile');
-export const patientGenomeTable = patientsIndexedDb.table('patientgenome');
+const patientProfileBulkAddRowsDemo = patientsIndexedDb.patientProfile.bulkPut([{
+  patient_id: '1',
+  patient_name: 'John Smith'
+},{
+  patient_id: '1',
+  patient_name: 'Hannah James'
+},{
+  patient_id: '3',
+  patient_name: 'Eric Mombasa'
+}]);
+const patientGenomeAddDemoRow = patientsIndexedDb.patientGenome.add({
+  rsid: '1',
+  patient_id: '1',
+  chromosome: '1',
+  position: '1',
+  genotype: 'AA'
+}); 
+const patientGenomeBulkAddRowsDemo = patientsIndexedDb.patientGenome.bulkPut([{
+    rsid: 'rs134242',
+    patient_id: '1',
+    chromosome: '4',
+    position: '7',
+    genotype: 'TC'
+  },{
+    rsid: 'rs13435',
+    patient_id: '1',
+    chromosome: '12',
+    position: '10',
+    genotype: 'AA'
+  },{
+    rsid: 'i12435',
+    patient_id: '1',
+    chromosome: '8',
+    position: '5',
+    genotype: 'GG'
+  },{
+    rsid: 'rs145345',
+    patient_id: '2',
+    chromosome: '8',
+    position: '2',
+    genotype: 'AG'
+  },{
+    rsid: 'i15435',
+    patient_id: '2',
+    chromosome: '5',
+    position: '14',
+    genotype: 'CC'
+}]); 
+
+export type { IPatientProfile, IPatientGenome, IPatientProfileAndGenome, ISnpPairsResearch, IFullReport}; 
 export { patientsIndexedDb };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,73 @@
-import React from 'react'; 
+import React, { useState } from 'react'; 
+import { render } from 'react-dom';
 import type {NextPage} from 'next'
-import Layout from '../components/Layout';  
-import AddPatientForm from "../components/AddPatientForm";
-import PatientList from "../components/PatientList";
+import Layout from '../components/Layout';   
 import dynamic from "next/dynamic"; 
+import Dexie, { type EntityTable } from 'dexie';
+import { useLiveQuery } from 'dexie-react-hooks'; 
+import { patientsIndexedDb, patientGenomeTable, patientProfileTable } from '@/database/db';
 
-const Home: NextPage = () => {
+const Home: NextPage = () => { 
+  const [status, setStatus] = useState('');
+  // const AddPatientForm = dynamic(() => import('@/components/AddPatientForm'), {
+  const UploadForm = dynamic(() => import('@/components/UploadForm'), {
+    ssr: false,
+  })
+
+  async function addPatientGenome() {
+    console.log('addPatientGenome');
+    try {
+      const id = await patientsIndexedDb.patientGenome.add({
+      // const id = await patientGenomeTable.add({
+        rsid: '1',
+        patient_id: '1',
+        chromosome: '1',
+        position: '1',
+        genotype: 'AA'
+      });
+      console.log('Added');
+      setStatus(`Gene variant successfully added. Got id ${id}`); 
+    } catch (error) {
+      console.log('Not added');
+      setStatus(`Failed to add patient gene variant: ${error}`);
+    }
+  } 
+
+  const patientProfiles = useLiveQuery( 
+    () => patientsIndexedDb.patientProfile.toArray()
+  );
+
+  const patientGenomes = useLiveQuery( 
+    () => patientsIndexedDb.patientGenome.toArray()
+  );
+
   return (
-    <Layout>  
-        <h2 className="table-header">Patient Database</h2>
-        <AddPatientForm /> 
+    <Layout>   
+    <p>{status}</p>
+      <h2 className="table-header">Patient Gene Variants</h2>
+      <UploadForm /> 
+      <hr />
+      {status}
+      <button onClick={addPatientGenome}>Add Patient Gene Variant</button>
+      <ul> 
+        {patientProfiles?.map((patientProfile) => (
+          <li key={patientProfile.patient_id}>
+            {patientProfile.patient_id}, 
+            {patientProfile.patient_name}
+          </li>
+        ))}
+      </ul>
+      <ul> 
+        {patientGenomes?.map((patientGenome) => (
+          <li key={patientGenome.rsid}>
+            {patientGenome.rsid}, 
+            {patientGenome.genotype}, 
+            {patientGenome.chromosome}, 
+            {patientGenome.position}
+            {patientGenome.patient_id}, 
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 };
