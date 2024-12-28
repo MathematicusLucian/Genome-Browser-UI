09ad8c658804ebf70eb9e385ee447559384943f0
MathematicusLucian <52294958+MathematicusLucian@users.noreply.github.com>
2024-12-28 20:04:18 +0000

    Logic for modal to close if click semi-transparent backdrop

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,44 +1,45 @@
 import React, { useContext } from "react";
+import { Button } from "./ui/button";
 
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
   content: any;
+  isOkButtonViisible?: any;
+  isCancelButtonViisible?: any;
 }
 
-// export const ModalComponent = React.forwardRef(
-//   (propsValues: ModalProps, ref) => {
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, content }) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, content, isOkButtonViisible, isCancelButtonViisible=true }) => {
     const closable = true; 
     const handleOk = () => {
-        
-        // Upload file
-        
         onClose();
     };
-    const handleCancel = () => {
-        if (!closable) return;
+    const handleCancel = (e) => { 
+        const classesWhereClickEventClosesModal = ['modal', 'close-button'];
+        const validClickEvent = classesWhereClickEventClosesModal.some(classOfItemClicked => e.target.className.includes(classOfItemClicked))
+        if (!closable || !validClickEvent) return;
         onClose();
     };
 
     return ( 
-        <div className={`modal fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center ${isOpen ? 'open' : ''}`}>
-                <div className="p-8 border w-96 shadow-lg rounded-md bg-white">
+        <div className={`modal fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center ${isOpen ? 'open' : ''}`}
+            onClick={handleCancel} 
+        >
+                <div className="p-8 border w-50 shadow-lg rounded-md bg-white dark:bg-gray-900">
                     <div className="text-center">
                         {content && (
-                            <div className="text-lg text-gray-500">
+                            <div className="text-md text-gray-900 dark:text-white">
                                 {content} 
                             </div>
                         )}
-                        <hr />
-                        <button className="close-button rounded bg-gray-900 px-3 py-1 mt-3 text-xs"
+                        {isOkButtonViisible && (<Button className="close-button rounded px-3 py-1 mt-3 bg-gray-900 dark:bg-white text-xs text-white dark:text-zinc-900"
                             onClick={handleOk}>
                             Upload
-                        </button>
-                        <button className="close-button rounded bg-gray-900 px-3 py-1 mt-3 text-xs"
+                        </Button>)}
+                        {isCancelButtonViisible && (<Button className="close-button rounded px-3 py-1 mt-3 bg-gray-900 dark:bg-white text-xs text-white dark:text-zinc-900"
                             onClick={handleCancel}>
                             Close
-                        </button> 
+                        </Button>)}
                     </div>
                 </div>
             <style jsx>{`
@@ -56,6 +57,12 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, content }) => {
                 div {
                     width: 80%;
                     padding: 1em;
+                }
+                h1 {
+                    font-size: 1rem;
+                }
+                h2, p, label, li {
+                    font-size: 0.8rem;
                 }
                 .modal.open {
                     transform: translateX(0);
