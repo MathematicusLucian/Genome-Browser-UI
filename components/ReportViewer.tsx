5cf121fbeb024a8fafe8ff776ea1d060fa7c0ab5
MathematicusLucian <52294958+MathematicusLucian@users.noreply.github.com>
2024-12-29 07:05:39 +0000

    ReportViewer component created

---
+++ components/ReportViewer.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React, { useState, useEffect, useContext, useMemo } from "react";  
+
+interface ReportViewerProps {
+    blah: string
+}
+
+const ReportViewer: React.FC<ReportViewerProps> = (props) => {  
+    return (
+        <>
+        
+        </>
+    );
+};
+
+export default ReportViewer;
