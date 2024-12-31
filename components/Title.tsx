a066ac1b252b2f8649656c9483edff767629147d
MathematicusLucian <52294958+MathematicusLucian@users.noreply.github.com>
2024-12-31 04:53:42 +0000

    Added dynamic button implemntation to Title component

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,14 +1,52 @@
+"use client";
+import Link from "next/link";
+import { Button } from "./ui/button";
 import { cn } from "../cn";
 
-export default function Title({ className, title }) {
+interface TitleProps {
+  className?: string,
+  title: string,
+  button?: any,
+  buttonText?: string,
+  icon?: any,
+  href?: string,
+  onClick?: any,
+}
+
+export default function Title({
+  className,
+  title,
+  button,
+  buttonText,
+  href = "",
+  onClick = () => {},
+  icon = true,
+}): TitleProps {
   return (
-    <h2
-      className={cn(
-        "text-center text-2xl font-semibold text-primary xl:text-3xl",
-        className,
-      )}
-    >
-      {title}
-    </h2>
+    <>
+      <h2
+        className={cn(
+          "text-center text-2xl font-semibold text-primary xl:text-3xl",
+          className,
+        )}
+      >
+        {title}
+      </h2>  
+      {button && buttonText ? (
+          href ? (
+            <Link href={href}>
+              <Button>
+                {buttonText}
+              </Button>
+            </Link>
+          ) : (
+            <Button 
+              onClick={onClick}
+            >
+              {buttonText}
+            </Button>
+          )
+        ) : null} 
+    </>
   );
 }
